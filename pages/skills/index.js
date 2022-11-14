0752114a1574898e6308643b483681d471124855
blackd44 <irabd44@gmail.com>
2022-11-15 00:54:03 +0200

    modifying colors - Day 2

--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Content from "../../components/content";
 
 const Skills = () => {
     return (
         <>
+            <Head>
+                <title>Skills | BlackD44</title>
+            </Head>
             <Content>
                 <h2>MY SKILLS</h2>
                 <div>
