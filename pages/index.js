1745423a085e9ce8471caddace33710691391c21
blackd44 <irabd44@gmail.com>
2022-11-14 13:12:02 +0200

    modifying landing page - Day 2

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,31 @@ import Link from 'next/link'
 import Content from '../components/content'
 import styles from '../styles/Home.module.css'
 
-export async function getServerSideProps() {
-  const content = {
-    title: 'Hi Ma Nigga',
-    body: [
-      `My name is Benn Dalton IRADUKUNDA, I'm a front-end developer based in Kigali, Rwanda. I have developed many types of front-ends from well known platforms, that was kidding, I designed my own projects.`,
-      `I'm passionate about... wait you don't know, check in the skills page.`
-    ],
-  }
+// export async function getServerSideProps() {
+//   const content = {
+//     title: 'Hi Ma Nigga',
+//     body: [
+//       `My name is Benn Dalton IRADUKUNDA, I'm a front-end developer based in Kigali, Rwanda. I have developed many types of front-ends from well known platforms, that was kidding, I designed my own projects.`,
+//       `I'm passionate about... wait you don't know, check in the skills page.`
+//     ],
+//   }
 
-  return {
-    props: {
-      content,
-    },
-  }
-}
+//   return {
+//     props: {
+//       content,
+//     },
+//   }
+// }
 
 export default function Home(props) {
-  const { content } = props
+
   return (
     <>
       <Content>
-        <h1>{content && content.title}</h1>
+        <h1>Hi Ma Nigga</h1>
         <div>
-          {content && content.body.map(each => (
-            <p key={each}>{each}</p>
-          ))}
+          <p>My name is Benn Dalton IRADUKUNDA, I'm a front-end developer based in Kigali, Rwanda. I have developed many types of front-ends from well known platforms, that was kidding, I designed my own projects.</p>
+          <p>I'm passionate about... wait you don't know, check in the skills page.</p>
         </div>
       </Content>
     </>
